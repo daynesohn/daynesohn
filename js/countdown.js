16b1dc1ba6e420b9f44bf7865255bd9a138da073
daynesohn <dayne.sohn@gmail.com>
2017-03-23 22:29:16 -0500

    Adding jQuery

--- js/countdown.js
+++ js/countdown.js
@@ -16,10 +16,10 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
+  // Output the result in an element with id="countdown"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-  // If the count down is over, write some text
+  // If the countdown is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "RIP Jimenez";
